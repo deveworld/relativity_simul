5844d879a6b5c7ee2b9df0555025f3ad5e0f70dd
World <world@worldsw.dev>
2025-06-20 23:34:43 +0900

    feat: 이벤트 기반 게임 개발을 위한 Ebiten 라이브러리 초기 설정

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"image/color"
+	"log"
+	"math"
+	"math/rand"
+	"time"
+
+	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
+	"github.com/hajimehoshi/ebiten/v2/inpututil"
+	"github.com/hajimehoshi/ebiten/v2/vector"
+)
+
+func main() {
+
+}
